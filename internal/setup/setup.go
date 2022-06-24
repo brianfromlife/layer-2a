2943f67d1719deff5225092cba70e3f596e1c71f
trevor atkinson <trevor@badappsdevelopment.com>
2022-06-24 16:50:25 -0600

    update build

--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -4,7 +4,20 @@ import (
 	"context"
 
 	envconfig "github.com/sethvargo/go-envconfig"
+	"github.com/trevatk/common/database"
+	"github.com/trevatk/common/server"
 )
+
+// Config environmental variables placeholder
+type Config struct {
+	Database database.Config
+	Server   server.Config
+}
+
+// ProvideConfig create new config object
+func ProvideConfig() *Config {
+	return &Config{}
+}
 
 // InvokeConfig process all environmental variables
 func InvokeConfig(config *Config) error {
